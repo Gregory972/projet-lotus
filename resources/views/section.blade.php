f907b8c89abce5850872a7657e72b859c32c37b4
Grégory Lagrandcourt <gregory.lgr7@gmail.com>
2025-07-01 16:24:32 -0400

    update meta field for responsive UI

--- resources/views/section.blade.php
+++ resources/views/section.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
     @vite('resources/css/app.css')
     @vite('resources/css/style.css')
